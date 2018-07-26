ac5ca33559cbe9e8441a990537e473586195bcfd
OTAVIO ODENIK <otavio.odenik@etec.sp.gov.br>
2018-07-26 11:39:55 -0300

    Classe Produto

---
+++ Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Quiron.LojaVirtual.Dominio.Entidade
+{
+    public class Produto
+    {
+        public int ProdutoId{ get; set; }
+        public string Nome { get; set; }      
+        public string Descricao  { get; set; }
+        public Decimal Preco  { get; set; }
+        public string  Categoria { get; set; }
+    }
+}
