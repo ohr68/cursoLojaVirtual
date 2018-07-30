9c32a364d9f106078c2fafbb359ee51b80d6d3cd
OTAVIO ODENIK <otavio.odenik@etec.sp.gov.br>
2018-07-30 10:59:58 -0300

    Pedido

---
+++ Quiron.LojaVirtual.Dominio/Entidade/Pedido.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Quiron.LojaVirtual.Dominio.Entidade
+{
+    public class Pedido
+    {
+        [Required(ErrorMessage = "Informe seu Nome!")]
+        [Display(Name = "Nome do cliente:")]
+        public string NomeCliente { get; set; }
+
+        [Display(Name = "Cep:")]
+        public string Cep { get; set; }
+
+        [Required(ErrorMessage = "Informe o Endereço!")]
+        [Display(Name = "Endereço:")]
+        public string Endereco { get; set; }
+
+        [Display(Name = "Complemento:")]
+        public string Complemento { get; set; }
+
+        [Required(ErrorMessage = "Informe a Cidade!")]
+        [Display(Name = "Cidade:")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "Informe o Bairro!")]
+        [Display(Name = "Bairro:")]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "Informe o E-mail!")]
+        [Display(Name = "E-mail:")]
+        [EmailAddress(ErrorMessage = "E-mail inválido!")]
+        public string Email { get; set; }
+
+        public bool EmbrulhaPresente { get; set; }
+    }
+}
