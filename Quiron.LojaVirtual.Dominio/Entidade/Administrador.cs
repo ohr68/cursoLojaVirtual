fe24cbb6ef1cb1f1ec9448b14afca4631b802938
OTAVIO ODENIK <otavio.odenik@etec.sp.gov.br>
2018-08-01 06:58:03 -0300

    Administrador

---
+++ Quiron.LojaVirtual.Dominio/Entidade/Administrador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Quiron.LojaVirtual.Dominio.Entidade
+{
+    public class Administrador
+    {
+        [Key]
+        public int AdminId { get; set; }
+        [Required(ErrorMessage = "Digite o Login")]
+        [Display(Name = "Login:")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Digite a senha")]
+        [Display(Name = "Senha:")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+        public DateTime UltimoAcesso { get; set; }
+    }
+}
